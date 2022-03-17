669f6066e4c587978562de44b7ff2b5cb7d7e0c8
Prakhar Mamgain <73053769+ProPrakhar332@users.noreply.github.com>
2022-03-17 22:25:19 +0530

    Add files via upload

--- src/screens/About.js
+++ src/screens/About.js
@@ -4,13 +4,23 @@ import "../screens/about.css"
 const About = () =>
 {
     return (
-      <>
+      <body>
         <div className="container emp-profile">
           <form method="">
-            <div className="row">
+            <div className="row mt-3">
               <div className="col-md-4">
                 <div className="profile-img">
                   <img src={pfp} alt="pfp"></img>
+                  <br></br>
+                  <br></br>
+                  <div className="bt">
+                    <input
+                      type="submit"
+                      className="profile-edit-btn"
+                      name="btnAddMore"
+                      value="Edit Profile"
+                    ></input>
+                  </div>
                 </div>
               </div>
 
@@ -18,31 +28,11 @@ const About = () =>
                 <div className="profile-head">
                   <h5>Prakhar Mamgain</h5>
                   <h5>Btech CST</h5>
-                  <p className="semester mt-3 mb-5">6</p>
-
+                  <h5>6th semester</h5>
+                  <br></br>
+                  <br></br>
+                  <br></br>
                   <ul className="nav nav-tabs" role="tablist">
-                    <li className="nav-item">
-                      <a
-                        className="nav-link"
-                        id="home-tab"
-                        data-toggle="tab"
-                        href="#home"
-                        role="tab"
-                      >
-                        Basic Info
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a
-                        className="nav-link"
-                        id="parent-tab"
-                        data-toggle="tab"
-                        href="#parent"
-                        role="tab"
-                      >
-                        Parents Info
-                      </a>
-                    </li>
                     <li className="nav-item">
                       <a
                         className="nav-link"
@@ -62,7 +52,7 @@ const About = () =>
                         href="#address"
                         role="tab"
                       >
-                        Address
+                        Communication Details
                       </a>
                     </li>
                     <li className="nav-item">
@@ -73,20 +63,33 @@ const About = () =>
                         href="#edu"
                         role="tab"
                       >
-                        Educational Details
+                        Academic Details
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link"
+                        id="home-tab"
+                        data-toggle="tab"
+                        href="#docs"
+                        role="tab"
+                      >
+                        Doccument Details
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link"
+                        id="home-tab"
+                        data-toggle="tab"
+                        href="#acc"
+                        role="tab"
+                      >
+                        Account Status
                       </a>
                     </li>
                   </ul>
                 </div>
-              </div>
-
-              <div className="col-md-2">
-                <input
-                  type="submit"
-                  className="profile-edit-btn"
-                  name="btnAddMore"
-                  value="Edit Profile"
-                ></input>
               </div>
             </div>
 
@@ -95,7 +98,6 @@ const About = () =>
 
               <div className="col-md-4">
                 <div className="profile-work">
-                  <p>Work Link</p>
                   <a
                     href="https://www.linkedin.com/in/prakhar-mamgain-746b341ab/"
                     target="_smile"
@@ -103,13 +105,16 @@ const About = () =>
                     LinkedIn
                   </a>
                   <br></br>
+                  <br></br>
                   <a href="https://github.com/ProPrakhar332" target="_smile">
                     GitHub
                   </a>
                   <br></br>
+                  <br></br>
                   <a href="https://leetcode.com/ProPrakhar/" target="_smile">
                     LeetCode
                   </a>
+                  <br></br>
                   <br></br>
                   <a
                     href="https://www.hackerrank.com/leviquackerman0"
@@ -117,6 +122,7 @@ const About = () =>
                   >
                     HackerRank
                   </a>
+                  <br></br>
                   <br></br>
                   <a
                     href="https://www.hackerearth.com/@mamgain332prakhar"
@@ -131,79 +137,37 @@ const About = () =>
               <div className="col-md-8 pl-5 about-info">
                 <div className="tab-content profile-tab" id="myTabContent">
                   <div
-                    className="tab-pane fade show active"
-                    id="home"
-                    role="tabpanel"
-                    aria-labelledby="home-tab"
-                  >
-                    <div className="row mt-3">
-                      <div className="col-md-6">
-                        <label>Name</label>
-                      </div>
-                      <div className="col-md-6">
-                        <p>Prakhar Mamgain</p>
-                      </div>
-                    </div>
-                    <div className="row mt-3">
-                      <div className="col-md-6">
-                        <label>Email</label>
-                      </div>
-                      <div className="col-md-6">
-                        <p>mamgain332prakhar@gmail.com</p>
-                      </div>
-                    </div>
-
-                    <div className="row mt-3">
-                      <div className="col-md-6">
-                        <label>DOB</label>
-                      </div>
-                      <div className="col-md-6">
-                        <p>22-09-2001</p>
-                      </div>
-                    </div>
-
-                    <div className="row mt-3">
-                      <div className="col-md-6">
-                        <label>Mobile No.</label>
-                      </div>
-                      <div className="col-md-6">
-                        <p>1454654654545145</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div
                     className="tab-pane fade"
                     id="parent"
                     role="tabpanel"
                     aria-labelledby="profile-tab"
                   >
-                    <div className="row">
-                      <div className="col-md-6">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Father's Name</label>
                       </div>
                       <div className="col-md-6">
                         <label>Prabodh Mamgain</label>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Father's Mobile Number</label>
                       </div>
                       <div className="col-md-6">
                         <label>9759010954</label>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Mother's Name</label>
                       </div>
                       <div className="col-md-6">
                         <label>Anita Mamgain</label>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Mother's Mobile Number</label>
                       </div>
                       <div className="col-md-6">
@@ -218,52 +182,36 @@ const About = () =>
                     role="tabpanel"
                     aria-labelledby="profile-tab"
                   >
-                    <div className="row">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Name</label>
+                      </div>
                       <div className="col-md-6">
-                        <label>DAte of Birth</label>
+                        <label>Prakhar Mamgain</label>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Father's Name</label>
+                      </div>
+                      <div className="col-md-6">
+                        <label>Mr. Prabodh Mamgain</label>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Mother's Name</label>
+                      </div>
+                      <div className="col-md-6">
+                        <label>Mrs. Anita Mamgain</label>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Date of Birth</label>
                       </div>
                       <div className="col-md-6">
                         <label>22-09-2001</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Gender</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>Male</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Nationality</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>Indian</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Blood Group</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>B-</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Aadhar Number</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>12216546854151</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>PAN Number</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>54646454654654</label>
                       </div>
                     </div>
                   </div>
@@ -274,36 +222,28 @@ const About = () =>
                     role="tabpanel"
                     aria-labelledby="profile-tab"
                   >
-                    <div className="row">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Email</label>
+                      </div>
                       <div className="col-md-6">
+                        <label>mamgain332prakhar@gmail.com</label>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Phone Number</label>
+                      </div>
+                      <div className="col-md-6">
+                        <label>9759010954</label>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Permanent Address</label>
                       </div>
                       <div className="col-md-6">
                         <label>74/15 Ashok Vihar,Dehradun</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Permanent Address PIN Code</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>248001</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Correspondence Address</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>same as permanent</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Correspondence Address PIN Code</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>248001</label>
                       </div>
                     </div>
                   </div>
@@ -314,40 +254,33 @@ const About = () =>
                     role="tabpanel"
                     aria-labelledby="profile-tab"
                   >
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>College Name</label>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>University Roll Number</label>
                       </div>
                       <div className="col-md-6">
-                        <label>Graphic Era University</label>
+                        <label>2015394</label>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>Marks in College(in %)</label>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>CGPA(in %)</label>
                       </div>
                       <div className="col-md-6">
                         <label>92</label>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6">
-                        <label>School Name</label>
-                      </div>
-                      <div className="col-md-6">
-                        <label>St. Joseph's Academy</label>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-6">
+
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Marks in XII(in %)</label>
                       </div>
                       <div className="col-md-6">
                         <label>92</label>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-md-6">
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
                         <label>Marks in X(in %)</label>
                       </div>
                       <div className="col-md-6">
@@ -355,12 +288,52 @@ const About = () =>
                       </div>
                     </div>
                   </div>
+                  <div
+                    className="tab-pane fade"
+                    id="acc"
+                    role="tabpanel"
+                    aria-labelledby="profile-tab"
+                  >
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Account Verification</label>
+                      </div>
+                      <div className="col-md-6">
+                        <label>Verified</label>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  <div
+                    className="tab-pane fade"
+                    id="docs"
+                    role="tabpanel"
+                    aria-labelledby="profile-tab"
+                  >
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>Aadhar Number</label>
+                      </div>
+                      <div className="col-md-6">
+                        <label>2090351486</label>
+                      </div>
+                    </div>
+                    <div className="row mt-3">
+                      <div className="head col-md-6">
+                        <label>PAN Number</label>
+                      </div>
+                      <div className="col-md-6">
+                        <label>65459759010954</label>
+                      </div>
+                    </div>
+                    
+                  </div>
                 </div>
               </div>
             </div>
           </form>
         </div>
-      </>
+      </body>
     );
 }
 export default About;
